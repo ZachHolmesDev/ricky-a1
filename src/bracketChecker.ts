b86915706be1462133a6f381e1ed9fb8837c18fa
zach <zholmes430@gmail.com>
2024-03-13 20:01:34 +1000

    Add bracketChecker implementation and update tsconfig.json

--- src/bracketChecker.ts
+++ src/bracketChecker.ts
@@ -1,20 +1,137 @@
-function bracketChecker(string: string): boolean {
+interface INode<T> {
+    value:T,
+    previous?:INode<T>,
+}
+
+
+class StackLL<T> {
+    public length: number;
+    private head?:INode<T>;
+
+    constructor() {
+        this.head = undefined
+        this.length = 0
+    }
+
+    push(item: T): void {
+        const newNode = {value: item} as INode<T>;
+        this.length++
+        
+        // if head undefined add new node to head 
+        if (!this.head) {
+            this.head = newNode
+            return
+        }
+        // link new node to current head 
+        newNode.previous = this.head
+        // change head to point to the new head 
+        this.head = newNode
+    }
     
-    
-    
-    return true
+    pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1)
+        const head = this.head as INode<T>
+
+        if (this.length === 0) {
+            this.head = undefined
+            return head?.value
+        }
+        
+        // point head to previous element
+        this.head = head.previous
+
+        // free mem in other lang here 
+
+        return head.value
+    }
+
+    peek(): T | undefined {
+        // if head return head if not return undefined 
+        return this.head?.value
+    }
+
+    wipe(): void {
+        this.head = undefined
+        this.length = 0
+    }
+}
+
+const bracketStack = new StackLL<string>()
+
+type openBracket = "(" | "[" | "{"
+type closedBracket = ")" | "]" | "}"
+
+// overly verbose isBracket functions cause learning TS 
+function isOpenBracket(char: string): char is openBracket {
+    return ["(", "[", "{"].includes(char);
+  };
+  
+function isClosedBracket(char: string): char is closedBracket {
+    return [")", "]", "}"].includes(char);
+  };
+  
+const bracketMap: {[key in openBracket]: closedBracket} = {
+    '(': ')',
+    '[': ']',
+    '{': '}',
+} 
+
+function bracketChecker(sequence: string): boolean {
+    // check if string empty
+    if (sequence.length === 0) {
+        return true
+    } else {
+        // loop over the string 
+        for (let i = 0; i < sequence.length; i++) {
+            const currentBracket = sequence[i]
+
+            // if bracket open push to stack
+            if (isOpenBracket(currentBracket)) {                
+                bracketStack.push(currentBracket)
+                continue
+            }
+
+            // if bracket closed compare to top of stack 
+            if (isClosedBracket(currentBracket)) {
+                // if closed bracket and stack empty return false
+                console.log(`found closed bracket : ${currentBracket} \nbracket stack length is ${bracketStack.length}`)
+
+                if (bracketStack.length === 0) {
+                    return false
+                }
+                // get last open bracket 
+                const lastOpenBracket = bracketStack.pop() as openBracket
+                // if bracket not match false otherwise all is well continue
+                if (bracketMap[lastOpenBracket] != currentBracket) {
+                    return false
+                }
+            }
+        }
+        // algorithm works correctly but the last test value in stringsToCheck array returns TRUE ??
+        // its cause the stack still has suff in it ??? wtf
+        // something simple but too tired to find right now  
+        bracketStack.wipe()
+        return true
+    }
 } 
 
 
 const stringsToCheck: string[] = [
     "({[]})",       // Balanced and nested correctly
-    "(}[)",         // Balanced but not nested correctly
+    "(}[)",         // Balanced but not nested correctly : FALSE
     "[][][][]",     // Single type of brackets, balanced
     "({[()]})",     // Mixed types, balanced
-    "([{}])}",      // Unbalanced, missing a closing bracket
-    "]{[{}]}",      // Unbalanced, missing an opening bracket
-    "",             // Empty string (should be considered correct as there are no unbalanced brackets)
+    "([{}])}",      // Unbalanced, missing a closing bracket : FALSE
+    "]{[{}]}",      // Unbalanced, missing an opening bracket : FALSE
+    "",             // Empty string (should be considered correct as there are no unbalanced brackets) 
     "[{({({[{}]}())})}]", // Long balanced string
-    "[{({({[{}](()})})}]", // Long unbalanced string
-    "}{[()]}{"      // Starts and ends incorrectly
+    "[{({({[{}](()})})}]", // Long unbalanced string : FALSE
+    "}{[()]}{"      // Starts and ends incorrectly : FALSE
 ]
+
+// test func over arr of str
+for (let i = 0; i < stringsToCheck.length; i++) {
+    console.log(`sequence "${stringsToCheck[i]}" is ballanced : ${bracketChecker(stringsToCheck[i])}\n`)
+}
+
+
--- src/test.ts
+++ src/test.ts
@@ -1,54 +1,61 @@
-interface Point {
-    x : number
-    y : number
-}
+// interface Point {
+//     x : number
+//     y : number
+// }
 
-function logPoint(p: Point) { 
-    console.log(`${p.x}, ${p.y}`)
-}
+// function logPoint(p: Point) { 
+//     console.log(`${p.x}, ${p.y}`)
+// }
 
-const point : Point = {x : 12, y : 34}
+// const point : Point = {x : 12, y : 34}
 
-logPoint(point)
+// logPoint(point)
 
 
 
-const point3 = { x: 12, y: 26, z: 89 };
-logPoint(point3); // logs "12, 26"
+// const point3 = { x: 12, y: 26, z: 89 };
+// logPoint(point3); // logs "12, 26"
  
-const rect = { x: 33, y: 3, width: 30, height: 80 };
-logPoint(rect); // logs "33, 3"
+// const rect = { x: 33, y: 3, width: 30, height: 80 };
+// logPoint(rect); // logs "33, 3"
  
-const color = { hex: "#187ABF" };
-// logPoint(color);
+// const color = { hex: "#187ABF" };
+// // logPoint(color);
 
 
-class VirtualPoint {
-    x: number;
-    y: number;
+// class VirtualPoint {
+//     x: number;
+//     y: number;
    
-    constructor(x: number, y: number) {
-      this.x = x;
-      this.y = y;
-    }
-  }
+//     constructor(x: number, y: number) {
+//       this.x = x;
+//       this.y = y;
+//     }
+//   }
    
-  const newVPoint = new VirtualPoint(13, 56);
-  logPoint(newVPoint); // logs "13, 56"
+//   const newVPoint = new VirtualPoint(13, 56);
+//   logPoint(newVPoint); // logs "13, 56"
 
-  interface Backpack<Type> {
-    add: (obj: Type) => void;
-    get: () => Type;
-  }
+//   interface Backpack<Type> {
+//     add: (obj: Type) => void;
+//     get: () => Type;
+//   }
 
-// This line is a shortcut to tell TypeScript there is a
-// constant called `backpack`, and to not worry about where it came from.
-declare const backpack: Backpack<string>;
+// // This line is a shortcut to tell TypeScript there is a
+// // constant called `backpack`, and to not worry about where it came from.
+// declare const backpack: Backpack<string>;
 
-// object is a string, because we declared it above as the variable part of Backpack.
-const object = backpack.get();
+// // object is a string, because we declared it above as the variable part of Backpack.
+// const object = backpack.get();
 
-// Since the backpack variable is a string, you can't pass a number to the add function.
-// backpack.add(23);
+// // Since the backpack variable is a string, you can't pass a number to the add function.
+// // backpack.add(23);
 
 
+// type openBracket = "(" | "[" | "{"
+// type closedBracket = ")" | "]" | "}"
+
+
+// const a = "o"
+
+// if (typeof a === openBracket)
